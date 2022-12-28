8500343ce8e53b5264d86db3e4897608dba18f0b
engrveju <engrveju@gmail.com>
2022-12-28 22:03:29 +0100

    Added updating ability

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 
-// import { useState } from 'react';
+import { useState } from 'react';
 import './index.css';
 import Employee from './components/Employee';
 
 function App() {
-
-
-  return (
-   <div className ="App bg-red-300">
   
-    <div className="flex flex-wrap justify-center">
-      <Employee name="John2" role="CTO"/>
-      <Employee name="Val" role="client"/>
-      <Employee  name="Peter" role="developer"/>
-      <Employee  name="Emma" role="Owner"/>
-      <Employee  name="Blessing" role="Banker"/>
-      <Employee name="James" role="Groil"/>
+    const [role, setRole] = useState("Engineer");
+    const [employees, setEmployee] = useState([
+      {name:"Chike",role:"Rider",id: 1},
+      {name:"Chike",role:"Rider",id: 2},
+      {name:"Chike",role:"Rider",id: 3},
+      {name:"Chike",role:"Rider",id: 4},
+      {name:"Chike",role:"Rider",id: 5},
+      {name:"Chike",role:"Rider",id: 6}]
+      )
+      function updateEmployee(id, newName, newRole){
+        console.log(id+ newName + newRole)
+      }
+console.log(role+setEmployee)
+console.log(employees)
+
+      const showEmployees =true;
+  return (
+      <div className ="App bg-red-300">
+        {showEmployees ? (
+          <>
+            <input
+              type="text"
+              onChange={(e)=>{
+                setRole(e.target.value);
+              }}
+            />
+      
+              <div className="flex flex-wrap justify-center">
+              {employees.map((employ)=>{
+                 return (
+                  <Employee 
+                  key={employ.id}
+                  id ={employ.id}
+                  name={employ.name} 
+                  role={employ.role}
+                  updateEmployee ={updateEmployee}
+                  />
+
+                 )
+                
+              })}
+              </div>
+    </>
+  ):(
+      <p>No Employees available</p>
+  )}
       </div>
-   </div>
 );
 }
 
--- src/components/EditEmployee.js
+++ src/components/EditEmployee.js
@@ -3,6 +3,10 @@ import React, { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 
 function EditEmployee(props) { 
+   
+     const [role,setRole] = useState(props.role);
+     const [name,setName] = useState(props.name);
+
 
     const [show, setShow] = useState(false);
 
@@ -26,30 +30,37 @@ function EditEmployee(props) {
         </Modal.Header>
         <Modal.Body>
         <div className="w-full max-m-ms">
-        <form id='editmodal' className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <form 
+            onSubmit={(e)=>{
+                e.preventDefault();
+                props.updateEmployee(props.id, name, role);
+            }}
+            id='editmodal' 
+            className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        >
             <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" for="name">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
                 name
             </label>
             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
                     id="name" 
                     type="text" 
-                    value={props.name}
+                    value={name}
                      onChange={(e)=>{
-                
+                        setName(e.target.value)
                      }}
             />
             </div>
             <div className="mb-6">
-            <label className="block text-gray-700 text-sm font-bold mb-2" for="Role">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="role">
                 Role
             </label>
             <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
                     id="role" 
                     type="text" 
-                    value={props.role}
+                    value={role}
                      onChange={(e)=>{
-                       
+                       setRole(e.target.value)
                      }}
                     />
             
@@ -64,7 +75,10 @@ function EditEmployee(props) {
             Close
          </button>
 
-          <button form="editmodal" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <button 
+                form="editmodal" 
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
             Save Changes
          </button>
         </Modal.Footer>
